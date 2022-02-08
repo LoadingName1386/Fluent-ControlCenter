7cfd4bcada6bc395d9a473a5e8d556ef27c7861e
LoadingName1386 <98937364+LoadingName1386@users.noreply.github.com>
2022-02-08 20:57:18 +0700

    Add files via upload

---
+++ Variables/lightmode.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
