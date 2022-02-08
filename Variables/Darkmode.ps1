293cd57fb35f287da5b925ae9180dd83450e30c6
Cariboudjan <Cariboudjan@gmail.com>
2022-02-08 08:21:29 -0700

    Cariboudjan changes

--- Variables/Darkmode.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
--- Variables/lightmode.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 1 -Type Dword -Force
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1 -Type Dword -Force
