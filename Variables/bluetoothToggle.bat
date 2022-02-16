cfb70a96c644723b50f38c47a23bef281b8388a6
LoadingName <98937364+LoadingName1386@users.noreply.github.com>
2022-02-16 23:18:34 +0700

    Add files via upload

--- Variables/bluetoothToggle.bat
+++ Variables/bluetoothToggle.bat
@@ -1,4 +1,4 @@
-@echo off
+echo off
 FOR /F "skip=2 tokens=2,*" %%A IN ('reg.exe query "HKLM\SYSTEM\CurrentControlSet\Services\bthserv\Parameters\BluetoothControlPanelTasks" /v "state"') DO set "VAR=%%B"
 
 if %VAR%==0x1 (goto ON)
